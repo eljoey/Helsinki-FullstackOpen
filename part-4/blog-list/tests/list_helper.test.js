df99057e439f40fdee8e85f4f2188bb95b6be85c
Joe Hayden <eljoeyy@gmail.com>
2019-08-31 19:12:03 -0500

    4.6 helper functions and unit tests, step4

--- part-4/blog-list/tests/list_helper.test.js
+++ part-4/blog-list/tests/list_helper.test.js
@@ -6,3 +6,88 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const blogs = [
+    {
+      title: 'test1',
+      author: 'test1 author',
+      url: 'test.com',
+      likes: 10
+    },
+    {
+      title: 'test2',
+      author: 'test2 author',
+      url: 'test.com',
+      likes: 20
+    },
+    {
+      title: 'test3',
+      author: 'test3 author',
+      url: 'test.com',
+      likes: 30
+    },
+    {
+      title: 'test4',
+      author: 'test4 author',
+      url: 'test.com',
+      likes: 40
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const oneBlog = blogs.slice(0, 1)
+
+    expect(listHelper.totalLikes(oneBlog)).toBe(10)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(100)
+  })
+})
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      title: 'test1',
+      author: 'test1 author',
+      url: 'test.com',
+      likes: 10
+    },
+    {
+      title: 'test2',
+      author: 'test2 author',
+      url: 'test.com',
+      likes: 40
+    },
+    {
+      title: 'test3',
+      author: 'test3 author',
+      url: 'test.com',
+      likes: 30
+    },
+    {
+      title: 'test4',
+      author: 'test4 author',
+      url: 'test.com',
+      likes: 20
+    }
+  ]
+  test("of an empty arry of blogs is 'no blogs given'", () => {
+    expect(listHelper.favoriteBlog([])).toEqual('no blogs given')
+  })
+
+  test('of an array with one blog returns that blog', () => {
+    const oneBlog = blogs.slice(0, 1)
+
+    expect(listHelper.favoriteBlog(oneBlog)).toEqual(oneBlog[0])
+  })
+
+  test('of an array of blogs is chosen correctly', () => {
+    expect(listHelper.favoriteBlog(blogs)).toEqual(blogs[1])
+  })
+})
--- part-4/blog-list/utils/list_helper.js
+++ part-4/blog-list/utils/list_helper.js
@@ -2,6 +2,30 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((acc, cur) => {
+        return acc + cur.likes
+      }, 0)
+}
+
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return 'no blogs given'
+
+  let likes = 0
+  let favorite = blogs.filter(blog => {
+    if (blog.likes > likes) {
+      likes = blog.likes
+      return blog
+    }
+  })
+
+  return favorite[favorite.length - 1]
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
